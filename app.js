58c7bfcb675c3ec9ef24eac2e8d329aab60c65be
Lori <148814155+Lbeckap@users.noreply.github.com>
2024-05-01 13:59:26 -0400

    updated project

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const hamburger = document.querySelector("#hamburger");
 
 const menu = document.querySelector("#menu");
+const moon = document.querySelector("#moon");
+const body = document.querySelector("body");
+const hLinks = document.querySelectorAll("#hLink");
 
 const bar1 = document.querySelector("#bar1");
 const bar2 = document.querySelector("#bar2");
 const bar3 = document.querySelector("#bar3");
 
 // when hamburger menu is clicked, the class list item "hidden" is toggled. This comes from the function menu classlist toggle
-hamburger.addEventListener("click", () => {
+hamburger.addEventListener("click", ()=>{
     menu.classList.toggle("hidden");
-    bar1.classList.toggle("bg-white");
-    bar2.classList.toggle("bg-white");
-    bar3.classList.toggle("bg-white");
+    bar1.classList.toggle("bg-indigo-900");
+    bar2.classList.toggle("bg-indigo-900");
+    bar3.classList.toggle("bg-indigo-900");
+});
+
+hLinks.forEach(link =>{
+    link.addEventListener("click", ()=>{
+        menu.classList.toggle("hidden");
+    });
+});
+
+moon.addEventListener("click", ()=>{
+    body.classList.toggle("dark")
 });
